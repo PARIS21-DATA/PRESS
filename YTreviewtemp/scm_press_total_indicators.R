6245a51c06e5a02f1da68b62481a4cbec52c751b
tian-y <y.tian.mailbox@gmail.com>
2022-03-15 14:44:50 +0100

    Update one script

--- YTreviewtemp/scm_press_total_indicators.R
+++ YTreviewtemp/scm_press_total_indicators.R
@@ -1,6 +1,5 @@
 rm(list = ls())
-press2021 = read_xlsx("./YTreviewtemp/PRESS 202112.xlsx") 
-
+load("./YTreviewtemp/press2021_from_excel.rds")
 press2021_short = press2021 %>%
   filter(countrySpecific ) %>%
   select(ListRecip, usd_commitment, CommitmentDate) %>%
@@ -8,15 +7,19 @@ press2021_short = press2021 %>%
   mutate(iso = countrycode(ListRecip, "country.name","iso3c")) %>%
   mutate(iso = ifelse(ListRecip == "Kosovo", "XKX", iso)) %>%
   filter(!is.na(iso))  %>%
-  filter(CommitmentDate <2020)
+  filter(CommitmentDate <2020) 
 
 
 press_summary = press2021_short %>%
-  filter(!is.na(usd_commitment)) %>%
-  group_by(iso,ListRecip, CommitmentDate ) %>%
+  filter(!is.na(usd_commitment)) %>% # listrecip contains multiple names for the same country
+  group_by(iso,
+           # ListRecip, 
+           CommitmentDate ) %>%
   summarise(total = sum(usd_commitment, na.rm = T))  %>%
   spread(CommitmentDate, total, fill = 0) %>%
-  gather(key = "year", value = "total", -iso, -ListRecip) %>%
+  gather(key = "year", value = "total", -iso
+         # , -ListRecip
+         ) %>%
   mutate(year = as.numeric(year)) %>%
   arrange(iso, year) %>%
   group_by(iso) %>%
@@ -26,15 +29,28 @@ press_summary = press2021_short %>%
          y3 = ifelse(y3 == 0, NA, y3)) %>%
   filter(!(is.na(total) & is.na(y3))) %>%
   rename(`121`=total, 
-         `123`=y3, 
-         country = ListRecip) %>%
-  filter(country!="Kosovo")
+         `123`=y3
+         # , 
+         # country = ListRecip
+         ) %>%
+  filter(iso !="XKX")
+
 
 press_summary[is.na(press_summary)] = 0
+
+unique(press_summary$iso) %>%
+  length
+
+
+press_summary = press_summary %>%
+  mutate(country = countrycode(iso, "iso3c", "country.name")) %>%
+  select(iso, country, year, `121`, `123`)
+
 
 save(press_summary , file = "./ytreviewtemp/scm_press_total_indicators.rds")
 
 load("./YTreviewtemp/scm_csaindicators.rds")
+
 write_csv(press_summary , file = "./YTreviewtemp/indicator-121-123.csv")
 write_csv(press2021_domain_sum_wide, file = "./YTreviewtemp/indicator-105-106-107-108-167.csv")
 
