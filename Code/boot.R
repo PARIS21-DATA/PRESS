017e7c99660fe3b839262873c21a8768de6757ff
tian-y <y.tian.mailbox@gmail.com>
2022-03-18 17:15:07 +0100

    clean environment

--- Code/boot.R
+++ Code/boot.R
@@ -1,16 +1,33 @@
-library(tidyverse)
-library(reshape2)
+# library(reshape2) #??? is it necessary?
 # library(splitstackshape)
-library(plyr)
+# library(plyr)
 # only one of the packages are needed
-library(cldr)
-library(cld2)
-library(cld3)
-library(beepr)
-library(slam)
-library(readxl)
-library(countrycode)
-library(zoo)
-library(readr)
+# library(cldr)
+# library(cld2)
+# library(cld3)
+# library(slam)
+
+
+# Load required libraries
+packages <-
+  c(
+    "tidyverse",
+    "readxl",
+    "openxlsx",
+    "reader",
+    "zoo", 
+    "countrycode", 
+    "beepr"
+  )
+# Install uninstalled packages
+lapply(packages[!(packages %in% installed.packages())], install.packages)
+lapply(packages, library, character.only = TRUE)
+rm(packages)
+
+# Set wd
+setwd(getwd())
+
+
+
 source("functions.R")
 
