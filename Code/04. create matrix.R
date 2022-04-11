e4a2d929a42be32cb5babc6f3d3c1f3d5eabbf89
tian-y <y.tian.mailbox@gmail.com>
2022-04-11 13:26:52 +0200

    Small modifications to the work flow of 04. 1. the threshold for Min.1 2. Adding a timer 3. remove a few lines to improve efficiency

--- Code/04. create matrix.R
+++ Code/04. create matrix.R
@@ -6,38 +6,26 @@ language <- "english"
 df_crs <- readRDS(crs_path)
 df_crs_original <- df_crs
 
-
+start_time = Sys.time()
 source("code/00.2 functions_thilo.R")
 source("code/00.3 functions_yu.R")
-# 
+
 # enthreshold = 30
 # frthreshold = 5
 # esthreshold = 1
 
-dictEN <- wlistV(lang)
-# dictFR <- wlist("fr")
-# dictES <- wlist("es")
+dict_lang <- wlistV(lang)
 
-# cw <- function(y) {
-#   unlist(
-#     lapply(y$content, function(x) length(unlist(strsplit(x, " "))) )
-#   )
-# }
-
-## 3.a. choose language
-Min.1 = 0.1
-Min.1.1 = 0.05
-
-
+# Split into to two groups
 
 df_crs_1 <- df_crs %>%
   filter( stats_filter 
-          # , language==lang 
+          # , language==lang
           )
 
 df_crs_0 <- df_crs %>%
   filter( !stats_filter 
-          # , language==lang)
+          # , language==lang
           )
 # which(df_crs_0$text_id %in% df_crs_1$text_id)
 ## 3.b. merge back by projectID to reverse previous split of project by language
@@ -65,15 +53,14 @@ df_crs_0 <- df_crs_0 %>%
 ## es=2, fr=5, en=40
 # Min.1 <- enthreshold/nrow(df_crs_1) ## only consider words that are in >5 SCB projects
 # Min.1 <- Min.2
-Min.1 <- 0.1 ## only consider words that are in >5 SCB projects ##??? not sure it's true
+Min.1 <- 0.1 ## only consider words that are in more than 10% of statistical projects
+Min.0 <- 0.1
 
 corpus_crs_1 <- preprocessingV(df_crs_1$description, language=language)
-
 dtm_crs_1 <- DTM(corpus_crs_1, Min=Min.1, Max=1)
-
 myDict <- dtm_crs_1$dimnames$Terms 
 
-# myDict = unique(c(myDict, dictEN))
+# myDict = unique(c(myDict, dict_lang))
 
 ## (3.c. english language only)
 # save(dtm_crs_1, file = "dtm_crs_1_en_2021.Rds")
@@ -87,23 +74,10 @@ nwords0 = tidy(corpus_crs_0) %>%
 # nwords[which(nwords != nwords0 )] 
 # nwords0[which(nwords != nwords0 )]
 dtm_crs_0 <- DTM(corpus_crs_0, dict = myDict)
-dtm_crs_0_1 <- DTM(corpus_crs_0, dict = dictEN)
-# 
-# corpus_crs_0_ <- preprocessing(df_crs_0$description, language=language)
-# nwords0 = c(cw(corpus_crs_0_))
-# dtm_crs_0_ <- DTM(corpus_crs_0_, dict = myDict)
-# nwords0_ = c(cw(dtm_crs_0_))
-# nwords0 = c(cw(corpus_crs_0))
-# nwords0 = c(cw(dtm_crs_0))
-# save(dtm_crs_0.p2, file="crs_preprocessed_en.p2_2021.RDs")
-# save(dtm_crs_0.p1, file="crs_preprocessed_en.p1_2021.rds")
-# save(dtm_crs_0.p3, file="crs_preprocessed_en.p3_2021.rds")
-
-## (3.d. english language only)
 
 list_high_freq_words_0 <- DTM(corpus_crs_0 , Min=Min.1, Max=1)$dimnames$Terms %>% unique
 myDict = myDict[!(myDict %in% list_high_freq_words_0)]
-myDict = unique(c(myDict, dictEN))
+myDict = unique(c(myDict, dict_lang))
 
 freq = DTM(corpus_crs_1, dict =myDict) %>%
   tidy %>%
@@ -119,12 +93,16 @@ nwords1 = nwords1 %>%
   left_join(freq) %>%
   mutate(count = ifelse(is.na(count), 0, count)) %>%
   mutate(percentage = count/total) 
+
 threshold = nwords1 %>%
   filter(count > 0) %>%
   .$percentage %>%
-  median
-mean(nwords1$percentage)
-median(nwords1$percentage)
+  mean
+
+print("mean:")
+mean(nwords1$percentage) %>% print()
+print("median:")
+median(nwords1$percentage) %>% print()
 
 # 
 # x = freq %>% row_sums()
@@ -153,11 +131,8 @@ positive_text_id = df_crs_0 %>%
   filter(document %in% list_identified) %>%
   .$text_id 
 
-# which(positive_text_id %in% df_crs_1$text_id)
-# 
-# df_crs_1$description[which(positive_text_id %in% df_crs_1$text_id)]
-# unique(positive_text_id)
-
-# length(positive_text_id)/nrow(df_crs_0)
 
 saveRDS(positive_text_id, file = crs_path_new)
+
+print(Sys.time() - start_time)
+# time spent for 1/40 of projects: almost 2 minutes
