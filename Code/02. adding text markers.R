91d924bdd1959b68b41d8541d37377f940e419e1
tian-y <y.tian.mailbox@gmail.com>
2022-05-10 12:02:04 +0200

    temporarily solve the mac issue with tolower invalid multibyte string

--- Code/02. adding text markers.R
+++ Code/02. adding text markers.R
@@ -21,13 +21,27 @@ cols_needed <- c("process_id",
 df_crs_raw <- df_crs_raw %>%
   select(all_of(cols_needed)) 
 
+# only needed when working on a Mac. Also not working well becausee it will convert utf8 to NA
+df_crs_raw = df_crs_raw %>%
+  mutate(projecttitle = iconv(projecttitle, "WINDOWS-1252", "UTF-8"),
+         shortdescription = iconv(shortdescription, "WINDOWS-1252", "UTF-8"),
+         longdescription= iconv(longdescription, "WINDOWS-1252", "UTF-8"))
+
 df_crs <- df_crs_raw %>%
   mutate(description_comb = paste(projecttitle, 
                                   shortdescription, 
                                   longdescription, 
                                   sep=". "), 
          description_comb = tolower(description_comb)
-         ) %>%
+         ) 
+
+# df_crs$description_comb[140:150]
+# df_crs_raw$longdescription[145]
+# a$longdescription[145]
+# df_crs_raw$longdescription[145]
+
+
+df_crs <- df_crs %>%
   mutate(text_id = as.numeric(as.factor(description_comb))) %>%
   ## add SCB identifier
   mutate(scb = ifelse(purposecode==16062,1,0), 
@@ -37,6 +51,9 @@ df_crs <- df_crs_raw %>%
          gen_marker = (gender %in% c(1, 2) & (!is.na(gender)))
          ) %>%
   select(-cols_needed[which(!cols_needed %in% c("process_id", "longdescription"))])
+
+
+
 
 df_crs_lang <- df_crs %>% 
   select(text_id, longdescription) %>%
