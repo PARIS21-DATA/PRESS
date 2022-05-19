1f4888616c46564890e44cfc5e1ad4107ff45094
tian-y <y.tian.mailbox@gmail.com>
2022-05-19 14:22:12 +0200

    create the sample for germany from all data Added 2 scripts Added 2 datasets

---
+++ Code/01.1 assign db_ref_DE.R
@@ -0,0 +1,56 @@
+### ---------------
+# start data cleaning 
+### 
+rm(list = ls())
+crs_path <- "./Data/Intermediate/crs_germany_sample.rds"
+crs_path_new <- paste0("./Data/Intermediate/crs", "01_1DE" , ".rds")
+df_crs_raw <- readRDS(crs_path)
+
+
+df_crs <- df_crs_raw %>%
+  select(
+    process_id, 
+    projecttitle, 
+    shortdescription, 
+    longdescription, 
+    crsid,
+    donorcode, 
+    year ,
+    usd_commitment, 
+    purposecode, 
+    usd_disbursement, 
+    recipientcode , 
+    usd_received
+  )
+
+
+df_crs$db_ref <- with(df_crs, 
+                      paste(
+                        crsid,
+                        donorcode, 
+                        year ,
+                        projecttitle,
+                        shortdescription, 
+                        longdescription,
+                        usd_commitment, 
+                        purposecode, 
+                        usd_disbursement, 
+                        recipientcode , 
+                        usd_received, # this is a new addition in 2022
+                        sep="_"
+                      )
+) %>%
+  as.factor %>%
+  as.numeric
+
+source("code/01.2 check uniqueness of db_ref.r")
+
+df_crs$db_ref = paste0("df_", df_crs$source, df_crs$db_ref) %>% as.factor()
+
+df_crs <- df_crs %>%
+  select(db_ref, process_id) %>%
+  inner_join(df_crs_raw)
+rm(df_crs_raw)
+gc()
+
+saveRDS(df_crs, file  = crs_path_new)
