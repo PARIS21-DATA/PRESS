25bbe3b81604617ec14c3759426578aa02c666da
tian-y <y.tian.mailbox@gmail.com>
2022-06-03 10:50:54 +0200

    adding temp office files to gitignore
    slightly modify encoding conversion
    
    german projects updated slightly too

--- Code/01.1 encoding coversion.R
+++ Code/01.1 encoding coversion.R
@@ -1,14 +1,21 @@
 rm(list = ls())
 gc()
-df_crs <- readRDS("Data/Raw/CRS/crs_full.rds")
+df_crs_raw <- readRDS("Data/Raw/CRS/crs_full.rds")
+df_crs <- df_crs_raw %>% 
+  # filter(donorname == "UN Women")
+  filter(channelcode == 41146)
+a = df_crs_raw$agencyname %>% unique
+grepl("women", a, ignore.case = T) %>% which
 
-df_crs_de <- df_crs %>% 
-  filter(donorname == "Germany")
 beep()
 # df_crs %>% select(year) %>% table()
-df_crs_de %>% select(agencyname) %>% unique
+# df_crs_de %>% select(agencyname) %>% unique
+df_crs %>% select(projecttitle) %>% head(10)
 infoRDS("Data/Raw/CRS/crs_full.rds")
 iconv(df_crs_de$agencyname,from = "CP1252", to = "UTF-8") %>% unique
+enc2utf8(df_crs_de$projecttitle) %>% head(10)
+
+
 
 convert_char_win2utf <- function(x) {
   # if(is.character(x)) {x <- iconv( x, from = "CP1252" , to = "utf-8")  }
