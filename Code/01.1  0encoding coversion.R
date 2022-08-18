24201863761075e4df6c9623070b2f9f171372cc
tian-y <y.tian.mailbox@gmail.com>
2022-08-18 18:50:55 +0200

    renaming the file temporarily

---
+++ Code/01.1  0encoding coversion.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+gc()
+df_crs_raw <- readRDS("Data/Raw/CRS/crs_full.rds")
+df_crs <- df_crs_raw
+
+beep()
+
+# df_crs %>% select(projecttitle) %>% head(10)
+infoRDS("Data/Raw/CRS/crs_full.rds")
+# iconv(df_crs$agencyname,from = "CP1252", to = "UTF-8") %>% unique
+enc2utf8(df_crs$projecttitle) %>% head(10)
+
+convert_char_win2utf <- function(x) {
+  # if(is.character(x)) {x <- iconv( x, from = "CP1252" , to = "utf-8")  }
+  if(is.character(x)) {x <- enc2utf8(x) }
+  return(x)
+}
+
+# is.character(df_crs_de$agencyname)
+
+df_crs_ls <- df_crs %>% 
+  lapply(convert_char_win2utf)
+beep(2)
+
+
+
+df_crs <- df_crs_ls %>% 
+  data.frame(stringsAsFactors = F)
+
+# checking if the characters are converted 
+df_crs %>% filter(donorname %in% c("France","Spain", "Switzerland", "Germany")) %>%
+  filter(longdescription != "") %>% 
+  pull(longdescription) %>%
+  sample() %>%
+  head(100)
+
+
+# saveRDS(df_crs, file = "Data/Raw/CRS/crs_utf8_full.rds", ascii = F)
+write_rds(df_crs, file = "Data/Raw/CRS/crs_utf8_full.rds")
+infoRDS("Data/Raw/CRS/crs_utf8_full.rds")
+rm(df_crs_raw)
+rm(df_crs_ls)
+
+beep()
