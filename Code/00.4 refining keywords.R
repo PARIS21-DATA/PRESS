837e4ee87391f0d566289cd393b5f437be231aa4
tian-y <y.tian.mailbox@gmail.com>
2022-08-19 18:14:36 +0200

    merge and reproduce three keyword lists

--- Code/00.4 refining keywords.R
+++ Code/00.4 refining keywords.R
@@ -1,6 +1,25 @@
+
+rm(list = ls())
+# lists merged: 
+# gender, small arm, statistics reduced
+
+file1 <- "data/keywords/gender_en.txt"
+file2 <- "data/keywords/gender_en_ja.txt"
+file_output <- "data/keywords/gender_en_final.txt"
+myDict = readLines(file1)
+myDict = c(myDict, readLines(file2))
+rm(file2, file1)
+myDict = myDict %>% 
+  trimws %>% 
+  unique
+
+
 keywords_matrix = sapply(myDict, FUN = function(y) {grepl(patter = y, x = myDict)}  )
 diag(keywords_matrix) = NA
 keywords_matrix = apply(keywords_matrix, 2, which)
 keywords_matrix = unlist(keywords_matrix) %>%unique
 keywords = myDict[-keywords_matrix]
 rm(keywords_matrix)
+
+writeLines(keywords, file_output)
+
