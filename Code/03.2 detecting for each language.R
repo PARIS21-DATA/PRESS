d40e1afa3dc7b5ae860ee7ef3672e3341ea0154d
tian-y <y.tian.mailbox@gmail.com>
2022-08-19 19:22:00 +0200

    improving the text search code to make it more efficient. the data set processed is much smaller somee redundant lines are removed
    Updated file locatioons for key words
    
    Something to check:
    The stemming, if it is not good. Does it affect how you reduced the keywords list?
    Also need to add multi-lingual functioons

--- Code/03.2 detecting for each language.R
+++ Code/03.2 detecting for each language.R
@@ -1,14 +1,15 @@
-df_crs <- df_crs_full %>%
-  filter(language_title == lang2analyse) ##??? to solve later
+df_crs <- df_crs_reduced %>%
+  filter(language_title == lang2analyse) %>% ##??? to solve later 
+  select(-language_title)
 
-# df_crs_backup <- df_crs
+df_crs_backup <- df_crs
 
 # df_crs <- df_crs %>%
 #   select(title_id, projecttitle_lower) %>%
 #   filter(!duplicated(title_id)) 
 
 # beep(4)
-list_keywords <- readLines(paste0("data/statistics_reduced_", lang2analyse, ".txt")
+list_keywords <- readLines(paste0("data/keywords/final/statistics_reduced_", lang2analyse, "_final.txt")
                            ,encoding = "UTF-8"
                            )  %>%
   trimws()
@@ -19,7 +20,7 @@ list_keywords <- readLines(paste0("data/statistics_reduced_", lang2analyse, ".tx
 # 
 # list_keywords <- list_keywords_simon
 
-list_keywords_gender <- readLines(paste0("data/gender_", lang2analyse, ".txt"), encoding = "UTF-8")  %>%
+list_keywords_gender <- readLines(paste0("data/keywords/final/gender_", lang2analyse, "_final.txt"), encoding = "UTF-8")  %>%
   trimws()
 
 
@@ -36,21 +37,14 @@ df_crs <- df_crs %>%
 print_time_diff(start)
 
 
-# 
-# tagged.results <- treetag(list_keywords, 
-#                           treetagger="manual", format="obj",
-#                           TT.tknz=FALSE , lang="en"
-#                           # ,
-#                           # TT.options=list(path="./TreeTagger", preset="en")
-#                           )
 
-list_blacklist <- readLines(paste0("data/blacklist_", lang2analyse, ".txt"), encoding = "UTF-8")  %>%
+list_blacklist <- readLines(paste0("data/keywords/final/demining_small_arms_", lang2analyse, "_final.txt"), encoding = "UTF-8")  %>%
   trimws()
 
 list_blacklist <- paste0(" ", list_blacklist, " ")
 
 
-list_acronyms <- readLines(paste0("data/statistics_reduced_acronyms_", lang2analyse, ".txt"), encoding = "UTF-8")  %>%
+list_acronyms <- readLines(paste0("data/keywords/final/statistics_reduced_acronyms_", lang2analyse, "_final.txt"), encoding = "UTF-8")  %>%
   trimws()
 list_acronyms <- paste0(" ", list_acronyms, " ")
 
@@ -63,7 +57,7 @@ df_crs <- df_crs %>%
   # mutate(text_detection_wo_mining = text_detection & !mining) %>%
   select(-projecttitle_lower, -projecttitle_stem)
 
-df_crs <- left_join(df_crs_backup, df_crs)
+# df_crs <- left_join(df_crs_backup, df_crs)
 
 rm(df_crs_backup)
 
