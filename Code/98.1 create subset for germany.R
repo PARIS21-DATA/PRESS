0ad6f4b8c69966d973c811551e8d7f6f62efc590
tian-y <y.tian.mailbox@gmail.com>
2022-08-25 16:18:12 +0200

    remove early scripts only used for comparisons and germany output

--- Code/98.1 create subset for germany.R
+++
@@ -1,6 +0,0 @@
-df_crs_backup <-  readRDS("Data/Raw/CRS/crs_full.rds")
-
-df_crs <- df_crs_backup %>%
-  filter(donorname == "Germany")
-
-saveRDS(df_crs, file = "Data/Intermediate/crs_germany_sample.rds")
--- Code/99.1 comparison simon vs p21 keywords.R
+++
@@ -1,5 +0,0 @@
-p21 = readRDS("Data/list_by_P21.rds") %>% mutate(P21 = "x")
-simon = read_rds("Data/list_by_simon.rds" ) %>% mutate(d4d = "x")
-
-comparison = full_join(p21, simon) %>% filter(!is.na(text_id))
-write_csv(comparison, file = "data/comparison.csv")
