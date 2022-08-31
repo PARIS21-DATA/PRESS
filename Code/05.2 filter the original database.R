46e8b2d0f534aebc2c5b163ef0ffc34563b96c3a
tian-y <y.tian.mailbox@gmail.com>
2022-08-31 17:18:45 +0200

    block many redundant lines

--- Code/05.2 filter the original database.R
+++ Code/05.2 filter the original database.R
@@ -1,15 +1,14 @@
 rm(list = ls())
 source("code/00. boot.R")
-
-
+start <- Sys.time()
 ### start merging filtered results to data
 ### load filtered results
 df_crs_wFilters <- read_rds("data/Intermediate/crs05_utf8_full.rds")
 ### load data with process ids 
-df_crs02 <- read_rds("data/Intermediate/crs02_utf8_full.rds")
+# df_crs02 <- read_rds("data/Intermediate/crs02_utf8_full.rds")
 
 names(df_crs_wFilters)
-names(df_crs02)
+# names(df_crs02)
 
 
 # all of df_crs columsn are actually within df_crs_wFilters
@@ -18,26 +17,27 @@ names(df_crs02)
 # df_crs02 contains text_id and process_id, which can be vital to latere stages, but it does not contain title ids. 
 # we should assume that crs01 and crs02 should have consistent process id. 
 # with the txt data added in late may. we should also assume that it has the most recent data. 
+# 
+# names(df_crs_wFilters)[!names(df_crs_wFilters) %in% names(df_crs02)]
+# names(df_crs02)[!names(df_crs02) %in% names(df_crs_wFilters)]
+# 
+# unique(df_crs02$process_id) %>% length
+# unique(df_crs_wFilters$process_id) %>% length
+# 
+# unique(df_crs02$text_id) %>% length
+# unique(df_crs_wFilters$text_id) %>% length
+# beep()
 
-names(df_crs_wFilters)[!names(df_crs_wFilters) %in% names(df_crs02)]
-names(df_crs02)[!names(df_crs02) %in% names(df_crs_wFilters)]
+# df_crs02_reduced <- df_crs02 %>% 
+#   select(text_id, process_id) 
 
-unique(df_crs02$process_id) %>% length
-unique(df_crs_wFilters$process_id) %>% length
-
-unique(df_crs02$text_id) %>% length
-unique(df_crs_wFilters$text_id) %>% length
-
-
-df_crs02_reduced <- df_crs02 %>% 
-  select(text_id, process_id) 
-
-df_crs02_filtered <- innerjoin(df_crs02, df_crs_wFilters)
+# df_crs02_filtered <- inner_join(df_crs02_reduced, df_crs_wFilters)
 # use innerjoin to see if it was successful
+# results: df_crs02 is a subset of filtered
 
 
-rm(df_crs02)
-rm(df_crs02_reduced, df_crs_wFilters)
+# rm(df_crs02)
+# rm(df_crs02_reduced, df_crs_wFilters)
 gc()
 
 
@@ -67,3 +67,8 @@ df_crs_01_filtered <- df_crs_wFilters %>%
 unique(df_crs_wFilters$process_id) %>% length
 unique(df_crs01$process_id) %>% length
 
+rm(df_crs_wFilters, df_crs01)
+
+write_rds(df_crs_01_filtered, file = "data/intermediate/crs05.2_utf8_full.rds")
+beep()
+print_time_diff(start)
