2916a3053c62f7988cb04674faf516fc6c08c2ad
tian-y <y.tian.mailbox@gmail.com>
2022-08-31 17:18:20 +0200

    replace nas in the desc filter

--- Code/05.1 remarking the projects.R
+++ Code/05.1 remarking the projects.R
@@ -1,4 +1,5 @@
 source("code/00. boot.R")
+start <- Sys.time()
 # pkgload:::unload("tidytext") # the stemmer in tidy text might be problematic for our steps here. 
 
 # source("code/00.2 functions_thilo.R")
@@ -21,11 +22,13 @@ print_time_diff(start)
 
 df_crs <- df_crs %>% 
   mutate(stats_filter_desc = ifelse(text_id %in% ls_ids, T, F)) %>% 
+  mutate(stats_filter  = replace_na(stats_filter, F),
+         stats_filter_desc = replace_na(stats_filter_desc, F)) %>% 
   mutate(stats  = stats_filter|stats_filter_desc)
 
-write_rds(df_crs, file = "Data/Intermediate/crs05_utf8_full.rds")
+# write_rds(df_crs, file = "Data/Intermediate/crs05_utf8_full.rds")
 
-df_crs <- read_rds("Data/Intermediate/crs05_utf8_full.rds")
+# df_crs <- read_rds("Data/Intermediate/crs05_utf8_full.rds")
 
 job_specific_suffix <- "_gen_utf8_full"
 # load("data/intermediate/crs04_lang_utf8_full.rdata")
@@ -37,7 +40,13 @@ ls_ids <- read_rds(crs_path_ids)
 
 df_crs <- df_crs %>% 
   mutate(gender_filter_desc = ifelse(text_id %in% ls_ids, T, F)) %>% 
-  mutate(gender_filter_both  = gender_filter_desc|text_filter_gender)
+  mutate(gender_filter_desc = replace_na(gender_filter_desc, F), 
+         text_filter_gender = replace_na(text_filter_gender, F)) %>% 
+  mutate(gender_filter_both  = gender_filter_desc|text_filter_gender) 
 
 
 write_rds(df_crs, file = "Data/Intermediate/crs05_utf8_full.rds")
+
+df_crs$gender_filter_both %>% table 
+beep()
+print_time_diff(start)
