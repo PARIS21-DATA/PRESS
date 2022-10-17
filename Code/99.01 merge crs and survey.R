fbee8d7bc343f1282895e54d64a737042fb26a09
tian-y <y.tian.mailbox@gmail.com>
2022-10-17 18:25:20 +0200

    updating reporter name merging

--- Code/99.01 merge crs and survey.R
+++ Code/99.01 merge crs and survey.R
@@ -1,21 +1,122 @@
-
-# setwd("~/Dropbox/PARIS21/PRESS/2021/")
 rm(list = ls())
+
+##### 
+# 1 loading data
+#@ load project data
 load("./data/Intermediate/06.1 crs and press with region and country code.rdata")
-df_reporters2 <- read_rds("data/auxiliary/reporters.rds")
-names(df_reporters2)
+## load reporter data
+df_reporters_press <- read_rds("data/auxiliary/reporters.rds")
+names(df_reporters_press)
 df_reporters <- read_csv("data/auxiliary/donor_codes_and_classifications.csv")
 
-df_reporters1 <- df_reporters %>% 
+df_reporters_concat_names <- read_csv(file = "Data/auxiliary/reporter_names_concatenation.csv")
+df_reporters_concat_names <- df_reporters_concat_names %>% 
+  filter(!is.na(short_names))
+
+#####
+# 2. reporter data reshape and reforme
+
+#####
+# 2.1. reduce the CH reporter data
+df_reporters <- df_reporters %>% 
   select(ReporterId, ReporterName, 
          crs_name_en, crs_code, 
          name_modification, ch_name,
          dac_member, dac_bi_member, mult_donor, mult_donor_and_eu, private_donor, non_dac_donor)  %>% 
   unique
+# remove the countries that does not belong to any group
+df_reporters <- df_reporters %>% 
+  mutate(total = rowSums( df_reporters[,7:12], na.rm = T)) %>% 
+  filter((total  != 0)|(!is.na(ReporterId)))  %>% 
+  select(-total)
 
-df_reporters1 <- df_reporters1 %>% 
-  mutate(total = rowSums( df_reporters1[,7:12], na.rm = T)) %>% 
-  filter(total  != 0)
+
+# which(is.na(press$donorcode))
+# which(is.na(crspress$donorcode))
+# which(is.na(press$donorname))
+# which(is.na(crspress$donorname))
+
+
+
+#######
+# 2.3 
+# reduce the press reporter dataset to only survey ones
+names(df_reporters_press)
+vec_survey_reporters <- df_survey %>% 
+  .$donorname %>% 
+  unique
+df_reporters_press <- df_reporters_press %>%
+  filter(!is.na(ReporterName), 
+         ReporterName %in% vec_survey_reporters) %>%
+  select(-crs, -press) %>%
+  # rename the variables for merging
+  rename(donorname_inlist = donorname, 
+         donorname = ReporterName) %>%  
+  unique
+
+rm(vec_survey_reporters)
+
+
+
+#####
+# 2.0. clean PRESS donor list 
+#there is duplicates for WB, AfDB and IMF, so before merging:
+df_reporters_press_4merge <- df_reporters_press %>%
+  group_by(donorname, donorname_unified) %>%
+  dplyr::summarise(donorcode = min(donorcode))
+
+
+
+
+
+# now you have ReporterName as "donorname", are they unified with any of the names in ch list? 
+
+######
+# 2.5 create a donor list from PRESS donors
+df_survey = df_survey %>%
+  left_join(df_reporters_press_4merge)   %>%
+  mutate(donorname = donorname_unified) %>%
+  select(-donorname_unified) 
+
+
+# merge the reporter with CRS project list by CRS donor code
+df_crs <-  df_reporters %>%
+  select(ch_name, donorcode = crs_code) %>%
+  filter(!is.na(donorcode)) %>% 
+  # select(donorcode,donorname = donorname_unified) %>%
+  unique %>%
+  right_join(df_crs) 
+# now df_crs has both crs donor name and "ch donor name"
+
+
+######
+# 2.2. examine which donors are not in the list
+var_crs_merge_quality <-  df_crs$ch_name %>% is.na %>% which
+if(length(var_crs_merge_quality) == 0) {
+  print("Great merge! All CRS project have corresponding donor code in the donor list!")
+} else {
+  "Problematic merge! Some CRS projects' donor are not in your donor list!!"
+}
+rm(var_crs_merge_quality)
+# it also means that all CRS codes are used
+
+# otherwise FIX: 
+# a <-  df_crs$ch_name %>% is.na %>% which
+# df_crs %>% select(donorname, donorcode) %>% slice(a) %>% unique %>% write_csv("Data/Analysis/reporters_not_in_list.csv")
+# fixed by adding the additional ones
+
+
+# check if there are unsuccessful merge
+var_crs_merge_quality <- df_survey %>% select(donorcode) %>% is.na %>% which
+if(length(var_crs_merge_quality) == 0) {
+  print("Great merge! All survey project have corresponding donor code in the donor list!")
+} else {
+  "Problematic merge! Some survey projects' donor are not in your donor list!!"
+}
+rm(var_crs_merge_quality)
+
+
+
 
 df_crs <- df_crs %>% 
   filter(stats_wo_infoSys)
@@ -26,50 +127,6 @@ df_crs$usd_disbursement_defl = df_crs$usd_disbursement_defl *1000000
 df_crs$commitmentdate = df_crs$commitmentyear
 
 df_survey$year = df_survey$startyear
-
-
-
-df_crs <-  df_reporters1  %>%
-  # filter(crs == 1) %>% # do not forget this step
-  select(ch_name, donorcode = crs_code) %>%
-  # select(donorcode,donorname = donorname_unified) %>%
-  unique %>%
-  right_join(df_crs) 
-
-df_crs$ch_name %>% is.na %>% which
-
-
-a <-  df_crs$ch_name %>% is.na %>% which
-df_crs$donorname[a]
-
-
-df_reporters_press = reporters %>%
-  filter(!is.na(ReporterName)) %>%
-  select(donorname = ReporterName, donorname_unified, donorcode) %>%
-  unique
-
-#there is duplicates for WB, AfDB and IMF, so before merging:
-df_reporters_press = df_reporters_press %>%
-  group_by(donorname, donorname_unified) %>%
-  dplyr::summarise(donorcode = min(donorcode))
-
-
-# which(reporters_press$donorname %>% duplicated )
-
-
-
-df_survey = df_survey %>%
-  left_join(df_reporters_press)   %>%
-  mutate(donorname = donorname_unified) %>%
-  select(-donorname_unified) 
-
-
-# which(is.na(press$donorcode))
-# which(is.na(crspress$donorcode))
-# which(is.na(press$donorname))
-# which(is.na(crspress$donorname))
-
-
 
 df_crs$source = "CRS"
 df_survey$source = "survey"
