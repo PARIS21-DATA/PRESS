aaf0fafb9750f9b9b99c2e93b5e5bfb796f1c897
tian-y <y.tian.mailbox@gmail.com>
2023-06-14 17:14:54 +0200

    no longer replace NAs with 0s for commitments and cost estimates

--- Code/06.1a initial process of survey data and disbursement split.R
+++ Code/06.1a initial process of survey data and disbursement split.R
@@ -18,17 +18,20 @@ path_output_disb_data <- paste0("data/Intermediate/06.1a survey data disbursemen
                                   ,".rds")
 
 df_survey <- read_rds(path_input)
+
+## 0.1 create ids
 df_survey <- df_survey %>% 
   mutate(db_ref = paste0("survey_", pressid)) %>%
   dplyr::rename(db_original_id = pressid) 
 
+## 0.2 filter out ones without donor names
 df_survey <-  df_survey  %>%
   rename(donorname = donor) %>% 
   filter(donorname!="",!is.na(donorname))
 
 
-df_survey$disbursement %>% head
-df_survey$commitment %>% head
+df_survey$disbursement %>% head #characters
+df_survey$commitment %>% head # characters
 df_survey$cost_estimate %>% head
 is.numeric(df_survey$cost_estimate)
 df_survey$disbursement %>% unique %>% length
@@ -39,12 +42,15 @@ df_survey$disbursement %>% unique %>% head
 ## 1. clean up
 
 df_survey <- df_survey %>% 
+  # replace empty strings with NA
   mutate(across(c( "commitment"), ~ ifelse(.x == "", NA, .x))) %>%
+  # there is not currency for these two variables so they should be in USD
   mutate(usd_commitment = commitment, 
          usd_costestimate = cost_estimate) %>%
+  # replace the commas in the string values
   mutate(across(c("usd_commitment"), ~ str_replace_all(string = .x, pattern = ",", replacement = "")))  %>% 
-  mutate(across(c( "usd_commitment"), ~ as.numeric(.x))) %>% 
-  mutate(across(c("usd_costestimate", "usd_commitment"), ~ replace_na(.x, 0)))
+  mutate(across(c( "usd_commitment"), ~ as.numeric(.x))) # %>% 
+  # mutate(across(c("usd_costestimate", "usd_commitment"), ~ replace_na(.x, 0)))
 
 # df_survey %>% 
 #   filter(is.na(usd_commitment), !is.na(commitment)) %>% 
@@ -58,15 +64,18 @@ df_survey <- df_survey %>%
 # df_survey$usd_costestimate[is.na(df_survey$usd_costestimate)] <- 0
 
 attributes(df_survey$commitment)$variable.labels <- "original commitment data collected from the survey"
-attributes(df_survey$usd_commitment)$variable.labels <- "numerised original commitment data collected from the survey, replaced NAs with 0"
+attributes(df_survey$usd_commitment)$variable.labels <- "numerised original commitment data collected from the survey, without replaced NAs with 0"
 attributes(df_survey$cost_estimate)$variable.labels <- "original cost estimate data collected from the survey"
-attributes(df_survey$usd_costestimate)$variable.labels <- "original cost estimate data collected from the survey, replaced NAs with 0"
+attributes(df_survey$usd_costestimate)$variable.labels <- "original cost estimate data collected from the survey, without replaced NAs with 0"
 
 # 2. column for usd disbursments
 
-df_disb <- tibble(disb = unique(df_survey$disbursement )) %>% 
+df_disb <- tibble(disb = unique(df_survey$disbursement)) %>% 
   mutate(id = 1:n()) 
 
+## split the disbursements if there are multiple entries
+
+## 2.1 first split strings to each entry, which will still contain year value and currency infos
 ls_disb <- df_disb$disb %>% 
   sapply( str_split, pattern = ";\\s+") 
 
@@ -76,6 +85,12 @@ df_disb_split_batch <- tibble(disb_batch = unlist(ls_disb),
 df_disb <- df_disb %>% 
   inner_join(df_disb_split_batch)
 
+df_disb <- df_disb %>% 
+  filter(disb_batch != "")
+
+rm(ls_disb)
+
+## 2.2 then split the information each year to year, value and currency
 
 df_disb_split_batch <- df_disb_split_batch %>% 
   select(disb_batch) %>% 
@@ -106,10 +121,12 @@ df_disb_split_2end <- bind_rows(ls_disb_split_2end) %>%
 df_disb <- df_disb %>% 
   inner_join(df_disb_split_2end)
 
-rm(ls_disb,
+rm(
    ls_disb_split_2end, 
    df_disb_split_2end, 
    df_disb_split_batch)
+
+## 2.3 split done, now format the values
 
 df_disb <- df_disb %>% 
   mutate(value = as.numeric(value), 
@@ -125,7 +142,7 @@ df_survey_disb %>%
   select(disbursement_currency) %>% 
   table
 
-
+# 3. save results
 saveRDS(df_survey, file = path_output_survey_data)
 saveRDS(df_survey_disb, file = path_output_disb_data)
 
