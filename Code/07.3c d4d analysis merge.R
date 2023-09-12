a320d270be7fbb76d96c7b6d3cb399fb9dd8e7b0
tian-y <y.tian.mailbox@gmail.com>
2023-09-12 17:05:46 +0200

    merging the white and black list of d4d, and retrospectively update analysis data, adding another dataset

---
+++ Code/07.3c d4d analysis merge.R
@@ -0,0 +1,26 @@
+source("code/00. boot.R")
+rm(list = ls)
+path_crs <- "data/intermediate/06.3 crs with donor code_2023.feather"
+
+path_blacklist <- "data/intermediate/07.3b d4d manual blacklist.feather"
+
+path_whitelist <- "data/intermediate/07.3a d4d manual additions.feather"
+
+df_crs <- read_feather(path_crs)
+df_blacklist <- read_feather(path_blacklist)
+df_whitelist <- read_feather(path_whitelist)
+
+df_crs <- df_crs %>% 
+  filter(!db_ref %in% df_blacklist$db_ref) 
+
+
+
+df_crs %>% 
+  group_by(year) %>% 
+  summarise(total = sum(usd_disbursement_defl, na.rm = T))
+
+df_whitelist %>% 
+  filter(db_ref %in% df_crs$db_ref) %>% 
+  nrow
+
+
