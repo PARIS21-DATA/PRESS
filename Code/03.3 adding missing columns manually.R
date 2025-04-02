a7f0bdb3b6014f3a2a904c99c3370b6f97c1497e
johannesabele <abelejohannes@gmail.com>
2025-04-02 09:49:28 +0200

    folder structure set up

--- Code/03.3 adding missing columns manually.R
+++
@@ -1,4 +0,0 @@
-df_crs <- df_crs %>% 
-  mutate(stat_manual_additions = grepl("National Agricultural Statistics Service|Integrated Household Survey|National Panel Survey|Living Standards Measurement Study Office|Core Agricultural and Rural Data Survey|Agricultural Integrated Survey", 
-                              longdescription, 
-                              T)) 
--- Documents/Unique identifiers for CRS.md
+++
@@ -1,8 +0,0 @@
-Normally, it is sufficient to distinguish the CRS data using these columns:
-
-db_original_id, 
-year, 
-dac_donorname, 
-recipientname, 
-projectnumber, 
-purposecode, 
